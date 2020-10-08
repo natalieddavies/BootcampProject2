4ed055251f43db80c832aa4aeac60e290879d52d
emilyshute <emilyshute116@gmail.com>
2020-10-08 21:24:48 +0800

    app.route upate

--- app.py
+++ app.py
@@ -12,34 +12,34 @@ from flask import Flask, jsonify
 engine = create_engine('postgresql://postgres:Emzy3314@localhost/DogsandCats')
 conn = engine.connect()
 
-##REFLECT EXISTING DB INTO NEW MODEL 
-#Base = automap_base()
-
-##reflect the tables
-#Base.prepare(engine, reflect=True)
-
-#Print all of the classes mapped to the Base
-#Base.classes.keys()
-
-# Assign the demographics class to a variable called `Dogs&Cats`
-#DogsCats = Base.classes.demographics
-
-# Create a session
-#session = Session(engine)
-
-#Use the session to query Demographics table and display the first 5 locations
-#for row in session.query().limit(5).all():
-    #print(row)
-
 ##Flask Setup
 app = Flask(__name__)
 
-##Flask Routes
+##Flask Route to ipynb jobs dataframe
 @app.route("/jobs")
 def jobs():
     jobs_df= pd.read_sql("select * from jobs", conn)
     jobs_data = jobs_df.to_json(orient='records')
     return jobs_data
+
+##Flask Route to ipynb latitude/longitude dataframe
+@app.route("/latlong")
+def latlong():
+    latLong_df= pd.read_sql("select * from latLong", conn)
+    latlong_data = latLong_df.to_json(orient='records')
+    return latlong_data
+
+##Flask Route to ipynb star signs jobs dataframe
+@app.route("/starsigns")
+def starsigns():
+    signs_df= pd.read_sql("select * from starsigns", conn)
+    signs_data = signs_df.to_json(orient='records')
+    return signs_data   
+
+##Create Landing page
+@app.route("/")
+def home():
+    return "/jobs for jobs, /latlong for locations, /starsigns for starsigns"    
 
 if __name__ == '__main__':
     app.run(debug=True)
